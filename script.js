279c17dd401d6135276cb71c916f33154a75b391
BitingBootleg <157678105+BitingBootleg@users.noreply.github.com>
2024-02-11 18:08:38 +0800

    Update script.js
    800px

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ noButton.addEventListener("click", function () {
     noCount++;
     const imageIndex = Math.min(noCount, MAX_IMAGES);
     changeImage(imageIndex);
-    resizeYesButton( innerwidth: 800 );
+    resizeYesButton(innerwidth: 800 );
     updateNoButtonText();
     if (noCount === MAX_IMAGES) {
       play = false;
